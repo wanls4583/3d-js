39da796ed824aea10f54c0235e0b664b709d0e41
lisong <409051987@qq.com>
2023-05-23 15:04:02 +0800

    feat: Scene

---
+++ src/scenes/Scene.js
@@ -0,0 +1,25 @@
+import Object3D from "../core/Object3D";
+
+class Scene extends Object3D {
+    constructor() {
+        super()
+
+        this.isScene = true
+        this.type = 'Scene'
+        this.background = null
+        this.environment = null
+    }
+    copy(source, recursive) {
+        super.copy(source, recursive)
+
+        if(source.background !== null) {
+            this.background = source.background
+        }
+
+        if(source.environment !== null) {
+            this.environment = source.environment
+        }
+
+        return this
+    }
+}
